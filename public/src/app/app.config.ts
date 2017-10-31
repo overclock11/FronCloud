9680397baa9b1136531df680a7ec67e5840e85d7
Julian Borray <jborray@smartsoftint.com>
2017-10-31 11:43:57 -0500

    Se modifica variable para worker client

--- public/src/app/app.config.ts
+++ public/src/app/app.config.ts
@@ -1,6 +1,6 @@
 // variable de configuacion
 var configuracion = {
   "endpoint":"cloud-entrega4.herokuapp.com",
-  "endpointWorker":"cloud-entrega4.herokuapp.com"
+  "endpointWorker":"worker-client.herokuapp.com"
 };
 export default configuracion;
